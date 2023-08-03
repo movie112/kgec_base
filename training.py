1b52bd5ccc5aaa19a67fb24bbe62d1e361db566e
YEONGHWA KIM <56140795+movie112@users.noreply.github.com>
2023-08-03 21:44:27 +0900

    Update training.py

--- training.py
+++ training.py
@@ -10,6 +10,7 @@ def training(args):
     
     model = Transformer_(args)
     model.apply(initialize_weights)
+    #
     model = model.to(args.device)
     print(f'model has {count_parameters(model):,} trainable parameters\n')
 
@@ -25,6 +26,7 @@ def training(args):
 
 
     best_valid_loss = np.inf
+    best_epoch = 0
     for epoch in range(args.epoch):
         printsave(args.result_path, f"====== {epoch} =======")
         model.train()
@@ -51,6 +53,7 @@ def training(args):
 
             batch_loss += loss.item()
             if (step+1) % 2000 == 0:
+                print('')
                 printsave(args.result_path, f'loss: {batch_loss / 2000}')
                 batch_loss = 0
 
@@ -77,73 +80,88 @@ def training(args):
                 total_loss += loss.item()
 
                 if (step+1) % 1000 == 0:
+                    print('')
                     printsave(args.result_path, f'loss: {batch_loss / 1000}')
                     batch_loss = 0
             printsave(args.result_path, f'total loss: {total_loss / len(valid_loader)}')
             if best_valid_loss > total_loss / len(valid_loader):
                 stop_cnt = 0
                 best_valid_loss = total_loss / len(valid_loader)
-                torch.save(model.state_dict(), os.path.join(args.model_dir, args.tokenizer_name, f'model.pt'))
-                printsave(args.result_path, 'saved model at', os.path.join(args.model_dir, args.tokenizer_name, f'model.pt'))
+                torch.save(model.state_dict(), os.path.join(args.model_dir, args.tokenizer_name, 'model.pt'))
+                printsave(args.result_path, 'saving model...')
+                best_epoch = epoch
             else:
                 stop_cnt += 1
                 if stop_cnt == args.patience:
                     printsave(args.result_path, 'early stopping')
                     break
+                printsave('stop_cnt:', stop_cnt)    
+            if epoch == 9:
+                torch.save(model.state_dict(), os.path.join(args.model_dir, args.tokenizer_name, 'model_10.pt'))
+            elif epoch == 14:
+                torch.save(model.state_dict(), os.path.join(args.model_dir, args.tokenizer_name, 'model_15.pt'))
+    print('best epoch:', best_epoch)
 
-def testing(args):
+def translation(args):
     printsave(args.result_path, "====== start testing =======")
     test_loader = make_loader(args, 'test')
     file_path = os.path.join(args.result_dir, args.tokenizer_name, f'pred.txt')
 
     model = Transformer_(args)
-    model.load_state_dict(torch.load(os.path.join(args.model_dir, args.tokenizer_name, f'model_.pt')))
+    model.load_state_dict(torch.load(os.path.join(args.model_dir, args.tokenizer_name, f'model.pt')))
     model = model.to(args.device)
     model.eval()
+
     with torch.no_grad():
-        ys_lst = []
+        tgt_lst = []
         bar = tqdm(enumerate(test_loader), total=len(test_loader))
-
         for i, batch in bar:
-            src = batch[0].T.to(args.device)
+            src = batch[0].T.to(args.device)  
             num_tokens = src.shape[0]
-            max_len = num_tokens + 5
-
-            unfinished_sequences = torch.ones(1,args.batch_size).to(args.device)
-            ys = torch.ones(1, args.batch_size).fill_(0).type(torch.long).to(args.device)
-            src_mask, _, src_padding_mask, _ = create_mask(src, ys, args.device)
-            context = model.encode(src, src_mask, src_padding_mask).to(args.device) # n, batch, dim
-            
-            for i in range(max_len-1):
-                tgt_mask = (generate_square_subsequent_mask(ys.size(0), args.device)
-                    .type(torch.bool))
-                out = model.decode(ys, context, tgt_mask)
-                out = out.transpose(0, 1)
-                prob = model.fc_out(out[:, -1])
-                _, next_word = torch.max(prob, dim = 1)
-
-                next_word = next_word * unfinished_sequences + (1-unfinished_sequences) * 2
-                ys = torch.cat([ys, next_word], dim=0)
-                ys = ys.type(torch.long)
-
-                unfinished_sequences = unfinished_sequences.mul((next_word != 1).long())
-                
-                if unfinished_sequences.max() == 0:
-                    break
-            ys = ys.T.tolist()
-            ys_lst += ys
-        print(f'saving pred.txt at {file_path}')
-        decoded_ys = [args.tokenizer.decode(ys) for ys in ys_lst ]
+            src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool)
+            tgt_tokens = beam_search_decode(
+                model,  src, src_mask, args, max_len=num_tokens + 5, beam_size=3, start_symbol=0)
+            tgt_tokens = tgt_tokens.tolist()
+            tgt_lst.append(args.tokenizer.decode(tgt_tokens))
+            # print('translated')
         with open(file_path, 'w') as f:
-            f.write('\n'.join(decoded_ys))
-    gold_path = os.path.join(args.result_dir, args.tokenizer_name, f'gold.txt')
-    pred_path = os.path.join(args.result_dir, args.tokenizer_name, f'pred.txt')
-
-    os.system(f'/home/yeonghwa/workspace/kgec/baseline/scripts/m2scorer.py {pred_path} {gold_path}')
+            f.write('\n'.join(tgt_lst))
 
 
-def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
+def beam_search_decode(model, src, src_mask, args, max_len, beam_size, start_symbol):
+    src = src.to(args.device)
+    src_mask = src_mask.to(args.device)
+
+    memory = model.encode(src, src_mask)
+
+    # 시작 토큰 추가
+    ys = torch.tensor([start_symbol], dtype=torch.long).to(args.device)
+
+    # 빈 beam list 생성
+    beam_list = [(0.0, ys, False)]  # (log probability, sequence)
+
+    for i in range(max_len - 1):
+        new_beam_list = []
+
+        for log_prob, sequence, finish in beam_list:
+            tgt_mask = generate_square_subsequent_mask(sequence.size(0), args.device).type(torch.bool).to(args.device)
+            out = model.decode(sequence.unsqueeze(1), memory, tgt_mask)
+            out = out.transpose(0, 1)
+            prob = model.fc_out(out[:, -1])
+            log_probs, next_words = torch.topk(F.log_softmax(prob, dim=1), beam_size, dim=1)
+
+            for j in range(beam_size):
+                new_sequence = torch.cat([sequence, next_words[0][j].unsqueeze(0)], dim=0)
+                new_log_prob = log_prob + log_probs[0][j].item()
+
+                if finish: # 끝났으면
+                    new_beam_list.append((log_prob, sequence, True))
+                else:
+                    if next_words[0][j].item() == 1: # EOS 토큰일 때, 결과 리스트에 추가
+                        new_beam_list.append((log_prob, sequence, True))
+                    else:# EOS 토큰이 아니면 다시 beam list에 추가
+                        new_beam_list.append((new_log_prob, new_sequence, False))
+
+        # beam_size에 따라 점수에 따라 정렬하고 상위 beam_size개를 선택
+        new_beam_list.sort(key=lambda x: x[0], reverse=True)
+        beam_list = new_beam_list[:beam_size]
